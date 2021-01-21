e6cee89f7e3779333b11468483390738e109ce53
Steven Smith <github@icodeforyou.com>
2021-01-21 10:11:52 -0500

    updated

--- test/persisted_objects_test.dart
+++ test/persisted_objects_test.dart
@@ -1,4 +1,4 @@
-// Copyright 2020 LTMM. All rights reserved.
+// Copyright 2021 LTMM. All rights reserved.
 // Uses of this source code is governed by 'The Unlicense' that can be
 // found in the LICENSE file.
 
